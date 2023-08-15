d99b09757f9074a02e1669bef0cc98fc48a03276
Kamran Hossain Topu <117673483+MuhammadTopu@users.noreply.github.com>
2023-08-16 03:15:35 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const url =  "https://api.quotable.io/random";
+
+let btn =document.querySelector(".get");
+btn.addEventListener("click",async ()=>{
+   let ress =await getQou();
+  let p = document.querySelector(".pp");
+   p.innerText = ress;
+
+let resss = await getAuth();
+   let p2=document.querySelector(".ppp");
+   p2.innerText = resss;
+ 
+})
+
+
+
+async function getQou() {
+  try {
+    const config= {
+      headers: {
+        Accept: "application/json"
+      }
+    }
+    let res= await axios.get(url,config);
+    return res.data.content;
+   
+    console.log(res);
+  }
+   catch(er) {
+    console.log("Error :",er);
+   }
+
+}
+   async function getAuth() {
+    try {
+      const config= {
+        headers: {
+          Accept: "application/json"
+        }
+      }
+      let res= await axios.get(url,config);
+      return res.data.author;
+     
+      
+    }
+     catch(er) {
+      console.log("Error :",er);
+     }
+     
+   
+  
+}
+const darkModeButton = document.getElementById('darkModeButton');
+const body = document.body;
+
+darkModeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  if (darkModeButton === "clicked") {
+    darkModeButton.innerText = "Light Mode";
+  }
+});
